9f79c39fb3570b828b073a9558455ffdbc86d7d3
milkteapuppy <156838365+milkteapuppy@users.noreply.github.com>
2026-01-24 14:01:08 -0500

    build: install postgreSQL
    update .env

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,10 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
+// middleware
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => res.send("Hello world!"));
 
