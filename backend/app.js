0337707871ad7c91ab047e281c89825e6aca374f
milkteapuppy <156838365+milkteapuppy@users.noreply.github.com>
2026-01-24 13:41:46 -0500

    build: install dotenv

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+// app.js
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("Hello world!"));
+
+const PORT = 3000;
+app.listen(PORT, (error) => {
+  if (error) {
+    throw error;
+  }
+  console.log(`App is listening on port ${PORT}!`);
+});
