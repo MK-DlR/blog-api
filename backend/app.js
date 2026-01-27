f4a08fa0cbe85ad84007beed87405dd6a195a3d1
milkteapuppy <156838365+milkteapuppy@users.noreply.github.com>
2026-01-27 10:44:23 -0500

    refactor: reorganize app.js code order
    set up controllers and routes folders

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,10 @@
 
 require("dotenv").config();
 const express = require("express");
-const app = express();
+const jwt = require("jsonwebtoken");
 const cors = require("cors");
+
+const app = express();
 
 // middleware
 app.use(cors());
