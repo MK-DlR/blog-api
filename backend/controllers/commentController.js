84c6e72f714ba70af33327d0dccb9a897210193f
milkteapuppy <156838365+milkteapuppy@users.noreply.github.com>
2026-01-28 12:21:42 -0500

    style: add commentController commenting

--- backend/controllers/commentController.js
+++ backend/controllers/commentController.js
@@ -6,8 +6,10 @@ const { prisma } = require("../lib/prisma");
 // get all comments
 exports.getAllComments = async (req, res) => {
   try {
+    // find all comments
     const comments = await prisma.comment.findMany();
 
+    // return all comments
     res.json({ comments });
   } catch (err) {
     res.status(500).json({ error: "Error fetching comments" });
@@ -17,14 +19,17 @@ exports.getAllComments = async (req, res) => {
 // get single comment
 exports.getCommentById = async (req, res) => {
   try {
+    // find specific comment
     const comment = await prisma.comment.findUnique({
       where: { id: parseInt(req.params.id) },
     });
 
+    // if specific comment not found
     if (!comment) {
       return res.status(404).json({ error: "Comment not found" });
     }
 
+    // return specific comment
     res.json({ comment });
   } catch (err) {
     res.status(500).json({ error: "Error fetching comment" });
@@ -34,7 +39,10 @@ exports.getCommentById = async (req, res) => {
 // create comment
 exports.createComment = async (req, res) => {
   try {
-    // code
+    // extract data
+    const comment = req.body;
+
+    // validate required fields
   } catch (err) {
     res.status(400).json({ error: "Error creating comment" });
   }
