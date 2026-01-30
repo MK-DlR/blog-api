6ea60ecec39a8ef25b87d0e0fe334ada0103239b
milkteapuppy <156838365+milkteapuppy@users.noreply.github.com>
2026-01-30 11:44:39 -0500

    feat: update schema to allow for optional images with blog posts
    begin user frontend DOM script set up

--- frontend-user/script.js
+++ frontend-user/script.js
@@ -5,4 +5,16 @@ import { API_URL } from "./config.js";
 // fetch and display posts
 fetch(`${API_URL}/posts`)
   .then((res) => res.json())
-  .then((data) => console.log(data));
+  .then((data) => {
+    const publishedPosts = data.posts.filter((post) => post.published);
+    const app = document.getElementById("app");
+
+    // create list container
+    const listContainer = document.createElement("ul");
+    // loop through publishedPosts
+    // for each post:
+    //   - create element
+    //   - set content (title, excerpt, date, etc.)
+    //   - append to container
+    // append container to app
+  });
